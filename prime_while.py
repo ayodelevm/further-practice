cfe929a627086116e8678b76b5c200da5e7c6904
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-01-15 13:17:57 +0100

    "First commit"

--- prime_while.py
+++ prime_while.py
@@ -1,11 +1,11 @@
 c = 2
 while c < 20:
-    a = 2
+    k = 2
     count = 0
-    while a < c:
-        if c % a == 0:
+    while k < c:
+        if c % k == 0:
             count = 1
-        a += 1
+        k += 1
     if count == 0:
         print(c, 'is a prime number')
     c += 1
