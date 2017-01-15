5d67d017a4ec5d2477d2db3f9be346b0ee74565c
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-01-15 10:57:26 +0100

    "Second commit"

--- Game_codings/state_capital_game.py
+++ Game_codings/state_capital_game.py
@@ -1,68 +1,61 @@
 """
-Hello, what's your name?
-Steve
-Welcome Steve!
+State and Capital Game
 
-Ready to play?
+The game generates a Nigerian state for you
+and you are required to correctly input the capital of that state.
 
-Yes
-
-What is the capital of Bayelsa State?
-
-Yenagoa
-
-Correct!
-
-What is the capital of Ondo State?
-
-Ado - Ekiti
-
-Wrong!
-
-The capital of Ondo State is Akure
-
-Game Over!
-
+The game keeps generating a new state until you eventually input a wrong capital and then the game ends.
 """
 
 d = {}
+import random
+
 with open("C:\Program Files (x86)\Python27\s and c.txt", "r") as f:
     for line in f:
         a = line.strip().split(' ' and '\t\t')
         k, v = a[0], a[1]
         d[str(k)] = v
 
-import random
+    
+    print("Hello, what's your name?")
+    MyName = input()
 
-print("Hello, what's your name?")
-MyName = input()
+    print("Welcome " + MyName + "!")
+    print("Ready to play? (yes/no)")
 
-print("Welcome " + MyName + "!")
-print("Ready to play? (yes/no)")
+    yes = Yes = True
+    no = No = False
+    ready_or_not = input()
+    
 
-ready_or_not = input()
-ready_or_not = str(ready_or_not)
+    for PlayerInput in ready_or_not:
+        if PlayerInput == no:
+            print("Thank you for your time. Try again another time")
+            break
+        if PlayerInput == yes:
+            continue
+        
+        
+        
+        comp_guess = 0
 
-comp_guess = 0
+        while comp_guess < 36:
+            x = random.choice(list(d.items()))
+            z = dict([(x)])
+            for k, v in z.items():
+                guess_key = k
+                guess_value = v = v.lower()
 
-while comp_guess < 36:
+                print("What is the capital of" + guess_key)
+                player_guess = input()
 
-    if ready_or_not == yes == Yes:
+                if player_guess == guess_value:
+                    print("Correct!")
 
-        x = random.choice(d.items())
-        guess_key = str(x.keys())
-        guess_value = str(x.value())
-
-        print("What is the capital of" + guess_key)
-        player_guess = input()
-
-        if player_guess == guess_value:
-            print("Correct!")
+                if player_guess != guess_value:
+                    break
 
         if player_guess != guess_value:
-            break
-
-if player_guess != guess_value:
-    print("Wrong")
-    print("\nThe capital of" + guess_key + "is" + guess_value)
-    print("Game Over!")
+            print("Wrong")
+            print("\nThe capital of" + guess_key + "is" + guess_value)
+            print("Game Over!")
